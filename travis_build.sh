da108897e794e6f02dc616267a2d79ca20df7145
Umkerius <SpiritOfTokamak@gmail.com>
2017-04-01 12:43:06 +0300

    Multiple cxx flags passing fix

--- travis_build.sh
+++ travis_build.sh
@@ -3,6 +3,6 @@ set -evx
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -Dadditional_cxx_flags=$CXX_FLAGS ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -Dadditional_cxx_flags="$CXX_FLAGS" ..
 cmake --build .
 ctest -V
